7f1ed5a3c0a4b377315a50df83e0ca54842a79b9
Alvaro <Alvaro@10.10.15.162>
2017-04-11 12:13:43 -0500

    animal class

--- CANNES/src/main/java/com/mpp/group/proj/model/Animal.java
+++ CANNES/src/main/java/com/mpp/group/proj/model/Animal.java
@@ -8,6 +8,37 @@ public class Animal {
 	private boolean neutered;
 	private LocalDate birth;
 	private String color;
-	 
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isNeutered() {
+		return neutered;
+	}
+	public void setNeutered(boolean neutered) {
+		this.neutered = neutered;
+	}
+	public LocalDate getBirth() {
+		return birth;
+	}
+	public void setBirth(LocalDate birth) {
+		this.birth = birth;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+		 
 
 }
