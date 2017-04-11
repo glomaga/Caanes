90595b7cc26b22b94e0b701cfb40f4933735efc2
glomaga <glomaga@10.10.14.35>
2017-04-11 13:52:48 -0500

    microchip

--- CANNES/src/main/java/com/mpp/group/proj/model/Microchip.java
+++ CANNES/src/main/java/com/mpp/group/proj/model/Microchip.java
@@ -3,9 +3,13 @@ package com.mpp.group.proj.model;
 import java.time.LocalDate;
 
 public class Microchip {
+	
+	private Animal animal;
 	private String description;
 	private String brand;
-	private LocalDate date;
+	private LocalDate implantdate;
+	private ImplantSite implantSite;
 	
+
 
 }
