c47395fd61e951f14aed11d0cb37b650119a75fa
Alvaro <Alvaro@10.10.15.162>
2017-04-11 12:05:32 -0500

    animal class

---
+++ CANNES/src/main/java/com/mpp/group/proj/model/Animal.java
@@ -0,0 +1,13 @@
+package com.mpp.group.proj.model;
+import java.time.LocalDate;
+
+public class Animal {
+	
+	private int id;
+	private String name;
+	private boolean neutered;
+	private LocalDate birth;
+	private String color;
+	 
+
+}
